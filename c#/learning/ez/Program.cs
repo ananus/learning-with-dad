463b22f915892bf4117122b71a5b1225bc37f6a3
ananus <yefimov.arthur@gmail.com>
2017-05-05 21:33:14 +0200

    binary conversion

---
+++ c#/learning/ez/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please insert un number: ");
+        string Input = Console.ReadLine();
+        Int64 num = Int64.Parse(Input.Trim());
+        string s=""; 
+        while (num > 0)
+        {
+            if (num % 2 == 0)
+            {
+                s = "0" + s;
+            }
+            else
+            {
+                s = "1" + s;
+            }
+            num /= 2;
+        }
+        Console.WriteLine(s);
+    }
+}
